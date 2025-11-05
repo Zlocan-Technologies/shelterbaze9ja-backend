69d8a30ecd0dc6c345dd3eb7b0e0432f1579dde7
emmanuelcoker <emmanuelcoker420@gmail.com>
2025-11-05 11:30:49 +0100

    rent saving fix

--- app/Repositories/RentSavingRepository.php
+++ app/Repositories/RentSavingRepository.php
@@ -215,7 +215,7 @@ class RentSavingRepository
         $user = $request->user();
         $saving = RentSaving::where('user_id', $user->id)->findOrFail($request->savings_id);
 
-        if($saving->target_amount > 100 && $request->amount < 100) {
+        if(($saving->target_amount - $saving->current_amount) > 100 && $request->amount < 100) {
             return ApiResponse::respond(
                 status: false,
                 statusCode: 400,
